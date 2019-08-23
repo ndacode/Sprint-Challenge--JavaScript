98c73181d9d9e2e2fb175ce6c19131909c62b801
Nicole Adams <NiceAdams@Nicoles-iMac.local>
2019-08-23 12:01:07 -0400

    Completed Functions. Completed Objects.

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,19 +6,34 @@
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
+function consume (a, b, cb){
+  return cb(a,b);
+};
+
+//Step 2: Create several functions to callback with consume();
+  // * Create a function named add that returns the sum of two numbers
+  // * Create a function named multiply that returns the product of two numbers 
+  // * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 
 
-/* Step 2: Create several functions to callback with consume();
-  * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
-*/
+const add = (num1, num2) =>{
+  return num1 + num2;
+}
+
+const multiply = (num1, num2) =>{
+  return num1 * num2;
+}
+
+const greeting = (first_name, last_name) => {
+  return `Hello ${first_name} ${last_name}, nice to meet you!`
+}
 
 
-/* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+
+//Step 3: Check your work by un-commenting the following calls to consume(): 
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -26,6 +41,8 @@
 // Explain in your own words why nestedfunction can access the variable internal.
 
 // Explanation: 
+    Nestedfunction //can access the variable internal because the parent function of the nested function has the information  nestedFunction needs to execute. The internal is in the parent scope, which the nested function is a child of, and so it has access to any variable defined within its parent's closure. 
+
 
 
 const external = "I'm outside the function";
